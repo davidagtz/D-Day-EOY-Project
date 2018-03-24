8814920a14d8dd59e03bbc05f229d70d5d375eb7
David Gutierrez <david.a.gutierrez99@gmail.com>
2018-03-23 19:42:52 -0500

    Running, skin color, and leg switch

--- src/Main.java
+++ src/Main.java
@@ -4,29 +4,61 @@ import javax.swing.Timer;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.awt.image.BufferedImage;
+import java.awt.image.PixelGrabber;
 import java.io.BufferedReader;
 import java.util.*;
 import java.io.*;
 
-public class Main extends JPanel implements ActionListener{
+public class Main extends JPanel implements ActionListener, KeyListener{
      public static long serialVersionUID = 0l;
      static JFrame frame;
-     static final int WIDTH = 800;
+     static final int WIDTH = 798;
      static final int HEIGHT = 600;
      static HashMap<Character, BufferedImage> font = new HashMap<>();
-     static final int PIXEL = 10;
+     static final int PIXEL = 6;
      static Timer timer;
-
+     static Player david;
+     static Player diego;
+     static Player jakob;
+     static double ang = 0;
+     static BufferedImage background;
+     static final Set<Integer> pressed = new HashSet<Integer>();
      public void paintComponent(Graphics g){
-          drawString(g, 10, 10, 5, "Diego like fat \n cock");
+          paintBackground(g);
+          diego.draw(g);
+          david.draw(g);
+          jakob.draw(g);
      }
-
+     public void paintBackground(Graphics g){
+          g.setColor(Color.CYAN);
+          g.fillRect( 0, 0, WIDTH, HEIGHT);
+     }
      public Main(String title){
+          //get Background
+          try{
+               background = ImageIO.read(new File("res/background.png"));
+          } catch(IOException e){
+               e.printStackTrace();
+          }
+
+          //Make Players
+          try{
+               david = new Player(ImageIO.read(new File("res/faces/david.png")));
+               diego = new Player(ImageIO.read(new File("res/faces/diego.png")));
+               jakob = new Player(ImageIO.read(new File("res/faces/jakob.png")));
+               david.move(0,david.faces.get(0).getHeight());
+          } catch(IOException e) {
+               e.printStackTrace();
+               System.exit(0);
+          }
+
           //Initialize Font
           try {
                for (int i = 'a'; i <= 'z'; i++)
-                    font.put((char) i, ImageIO.read(new File("font/"+(char) i + ".png")));
+                    font.put((char) i, ImageIO.read(new File("res/font/"+(char) i + ".png")));
           } catch (IOException e){
                e.printStackTrace();
                System.exit(0);
@@ -38,13 +70,50 @@ public class Main extends JPanel implements ActionListener{
           frame.setResizable(false);
           this.setPreferredSize(new Dimension(WIDTH, HEIGHT));
           frame.add(this);
+          frame.addKeyListener(this);
           frame.pack();
           frame.setVisible(true);
 
           //Timer setup for repaint()
-          timer = new Timer(20, this);
+          timer = new Timer(30, this);
           timer.start();
 
+     }
+     public void keyReleased(KeyEvent e){
+          if(pressed.remove(e.getKeyCode())){
+               int c = e.getKeyCode();
+               if(c == KeyEvent.VK_A || c == KeyEvent.VK_D) {
+                    david.setRunning(0);
+               }
+               if(c == KeyEvent.VK_LEFT || c == KeyEvent.VK_RIGHT) {
+                    diego.setRunning(0);
+               }
+          }
+
+     }
+     public void keyTyped(KeyEvent e){
+
+     }
+     public void keyPressed(KeyEvent e){
+          pressed.add(e.getKeyCode());
+          for(int code : pressed) {
+               if (code == KeyEvent.VK_LEFT) {
+                    diego.move(-1, 0);
+                    diego.setRunning(-1);
+               }
+               else if (code == KeyEvent.VK_RIGHT) {
+                    diego.move(1, 0);
+                    diego.setRunning(1);
+               }
+               if (code == KeyEvent.VK_A) {
+                    david.move(-1, 0);
+                    david.setRunning(-1);
+               }
+               else if (code == KeyEvent.VK_D) {
+                    david.move(1, 0);
+                    david.setRunning(1);
+               }
+          }
      }
      public static void main(String[] a){
           new Main("D-Day");
@@ -76,5 +145,22 @@ public class Main extends JPanel implements ActionListener{
      }
      public void actionPerformed(ActionEvent e){
           repaint();
+     }
+     public static  BufferedImage reverse(BufferedImage img){
+          BufferedImage rev = copy(img);
+          for(int r = 0; r < img.getHeight(); r++){
+               for(int c = 0; c < img.getWidth(); c++){
+                    int col = img.getRGB(img.getWidth()-1-c, r);
+                    rev.setRGB(c, r, col);
+               }
+          }
+          return rev;
+     }
+     public static BufferedImage copy(BufferedImage source){
+          BufferedImage b = new BufferedImage(source.getWidth(), source.getHeight(), source.getType());
+          Graphics g = b.getGraphics();
+          g.drawImage(source, 0, 0, null);
+          g.dispose();
+          return b;
      }
 }
--- src/Player.java
+++ src/Player.java
@@ -1,22 +1,69 @@
+import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Player {
      ArrayList<ArrayList<Rectangle>> body = new ArrayList<>();
-     BufferedImage face;
-     ArrayList<BufferedImage> bodies = new ArrayList<>();
+     HashMap<Integer, BufferedImage> faces;
+     HashMap<Integer, BufferedImage> bodies;
      int r;
      int c;
-     public Player(BufferedImage f){
-          face = f;
-//          bodies.add();
-//          bodies.add();
-//          bodies.add();
+     int dir = 0;
+     int leg = 1;
+     int dc  = -12904;
+     int dc2 = -1590900;
+     public Player(BufferedImage f) throws IOException{
+          //Makes faces for a dir
+          faces = new HashMap<>();
+          faces.put( 0, f.getSubimage( 0, 0, f.getWidth(), f.getHeight()/2));
+          faces.put( 1, f.getSubimage( 0, f.getHeight()/2, f.getWidth(), f.getHeight()/2));
+          faces.put(-1, Main.reverse(f.getSubimage( 0, f.getHeight()/2, f.getWidth(), f.getHeight()/2)));
+
+          //Makes bodies for a dir
+          bodies = new HashMap<>();
+          bodies.put( 0, match(ImageIO.read(new File("res/body/body.png")), faces.get(0)));
+          bodies.put( 1, match(ImageIO.read(new File("res/body/bodyright.png")), faces.get(0)));
+          bodies.put( 2, match(ImageIO.read(new File("res/body/bodyleft.png")), faces.get(0)));
+          bodies.put(-1, Main.reverse(match(ImageIO.read(new File("res/body/bodyright.png")), faces.get(0))));
+          bodies.put(-2, Main.reverse(match(ImageIO.read(new File("res/body/bodyleft.png")), faces.get(0))));
      }
+
+     public BufferedImage match(BufferedImage body, BufferedImage face){
+          int tf = face.getRGB( face.getWidth() / 2, face.getHeight()-1);
+          int tf2 = face .getRGB( face.getWidth() / 2, face.getHeight() / 2);
+          for(int y = 0; y < body.getHeight(); y++)
+               for(int x = 0; x < body.getWidth(); x++){
+                    if(body.getRGB(x, y) == dc)
+                         body.setRGB(x, y, tf);
+                    else if ( body.getRGB(x, y) == dc2)
+                         body.setRGB(x, y, tf2);
+               }
+          return body;
+     }
+
      public void move(int dirx, int diry){
           r+=diry;
           c+=dirx;
+          change();
      }
 
+     public void change(){
+          leg++;
+          leg %= 2;
+     }
+
+     public void setRunning(int running) {
+          dir = running;
+     }
+
+     public void draw(Graphics g){
+          BufferedImage face = faces.get(dir);
+          BufferedImage body = bodies.get((int) (Math.signum(dir) * (Math.abs(dir) + leg)));
+          g.drawImage(body, (c + face.getWidth()/2 - body .getWidth() / 2) * Main.PIXEL,  (r + face.getHeight() - 2) * Main.PIXEL, body.getWidth() * Main.PIXEL, body.getHeight() * Main.PIXEL, null);
+          g.drawImage(face, c * Main.PIXEL, r * Main.PIXEL, face.getWidth() * Main.PIXEL, face.getHeight() * Main.PIXEL, null);
+     }
 }
